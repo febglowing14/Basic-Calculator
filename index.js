88456abd38c7f1139ea5413680e6421bb7818932
febglowing14 <gopikavijayarajan@gmail.com>
2024-10-13 04:18:49 +0530

    updated square and sqaureroot

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
-let num1 = 8
-let num2 = 2
-document.getElementById("num1-el").textContent = num1
-document.getElementById("num2-el").textContent = num2
 
+//document.getElementById("num1-el").textContent = num1
+let num1, num2 
+//document.getElementById("num2-el").textContent = num2
 let  sum = document.getElementById("sum-el")
 console.log(sum)
+function getvalues(){
+     num1 = parseFloat(document.getElementById("num1").value);
+     num2 = parseFloat(document.getElementById("num2").value);
 
-function add() {
+}
+function add() { 
+    getvalues()
     let result = num1 + num2
     sum.textContent = "SUM : "+ result
     
 }
 
-function multiple() {
+function multiply() {
+    getvalues()
     let result = num1 * num2
     sum.textContent ="Multiple : "+result
 }
 
-function subtarct() {
+function subtract() {
+    getvalues()
     let result = num1 - num2
     sum.textContent ="Subtract : "+result
 }
 
 function divide() {
+    getvalues()
     let result = num1 / num2
     sum.textContent ="Divide : "+result
+}
+function square(){
+    getvalues()
+    let result = num1 * num1
+    sum.textContent = "Square :" + result 
+
+}
+function squareRoot() {
+    getvalues();
+    let result = Math.sqrt(num1);
+    sum.textContent = "SQUARE ROOT: " + result;
 }
